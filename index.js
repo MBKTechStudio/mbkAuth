d0862c9047cd0064a4ff2fccc144a5a37468ecd5
Muhammad Bin Khalid <83428194+MIbnEKhalid@users.noreply.github.com>
2025-04-10 14:54:17 +0000

    feat: Update environment configuration and enhance login functionality with 2FA support

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import router from "./lib/main.js";
-
 dotenv.config();
-
+export { authenticate } from "./lib/auth.js";
+export { dblogin } from "./lib/pool.js";
 export default router;
